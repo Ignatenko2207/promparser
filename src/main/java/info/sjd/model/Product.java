7498aa862ceda67fb7089db57356449e392d3766
Alex <ignatenko2207@gmail.com>
2019-10-11 09:56:57 +0300

    Add necessary classes

---
+++ src/main/java/info/sjd/model/Product.java
@@ -0,0 +1,17 @@
+package info.sjd.model;
+
+import lombok.*;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    private String code;
+    private String name;
+    private BigDecimal price;
+    private String avialability;
+}
